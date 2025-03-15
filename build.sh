517f8150c36477e334fa4a9ea7316dacf4c054d8
deltaclock <24370162+deltaclock@users.noreply.github.com>
2025-03-15 18:32:02 +0200

    unneeded file

--- build.sh
+++
@@ -1,7 +0,0 @@
-if [ "$CF_PAGES_BRANCH" == "master" ]; then
-    hugo -b $CF_PAGES_URL
-elif [ "$CF_PAGES_BRANCH" == "staging" ]; then
-    hugo -D -b $CF_PAGES_URL
-else
-    echo "branch: $CF_PAGES_BRANCH"
-fi
